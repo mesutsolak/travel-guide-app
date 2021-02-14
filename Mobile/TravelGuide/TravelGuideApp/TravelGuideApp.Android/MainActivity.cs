5c51fd1cf447c15e100e3a205bef979b1863758c
Mesut Solak <mesut.solak@idesse.com.tr>
2021-02-14 18:41:04 +0300

    fix: uygulamanın IOS ve Android'de sıkıntısız bir şekilde çalıştırılması sağlandı.

--- Mobile/TravelGuide/TravelGuideApp/TravelGuideApp.Android/MainActivity.cs
+++ Mobile/TravelGuide/TravelGuideApp/TravelGuideApp.Android/MainActivity.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
+using Prism;
+using Prism.Ioc;
 
 namespace TravelGuideApp.Droid
 {
